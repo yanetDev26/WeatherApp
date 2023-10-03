b9cd1ad93e9bd951507766968624b4e5d30df0a9
@yanetrodriguez26 <yanet-rodriguez@outlook.com.ar>
2023-10-02 23:03:00 -0300

    Update

---
+++ app/src/main/java/com/weatherapp/data/remote/WeatherApi.kt
@@ -0,0 +1,15 @@
+package com.weatherapp.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
+    suspend fun getWeatherData(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double
+    ): WeatherDto
+
+
+}
